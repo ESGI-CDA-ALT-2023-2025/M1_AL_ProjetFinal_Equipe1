4ca8809fe768af1b50fccf8490882ed5acd2c4a1
GALLIC-A <axel.gallic@orange.fr>
2023-12-21 10:58:18 +0100

    Ajout de Swagger

--- DVF/src/main/java/fr/esgi/dvf/controller/DonneeFonciereRestController.java
+++ DVF/src/main/java/fr/esgi/dvf/controller/DonneeFonciereRestController.java
@@ -4,19 +4,23 @@ import lombok.AllArgsConstructor;
 import org.springframework.validation.annotation.Validated;
 import org.springframework.web.bind.annotation.*;
 
+import io.swagger.v3.oas.annotations.Operation;
+import io.swagger.v3.oas.annotations.Parameter;
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @RestController
 @AllArgsConstructor
 @RequestMapping("")
 @Validated
 public class DonneeFonciereRestController {
 
+    @Operation(summary = "Route à laquelle il faut renseigner une longitude, une latitude et un rayon en mètre pour obtenir en retour un PDF listant toutes les transactionsqui se sont effectuées dans la zone demandée.")
     @GetMapping("/calculer")
     public String calculer(
-            @RequestParam(name="longitude", defaultValue="0") Double longitude,
-            @RequestParam(name="latitude", defaultValue="0") Double latitude,
-            @RequestParam(name="rayon", defaultValue="0") Double rayon
+            @Parameter(description = "Longitude", example = "0", schema = @Schema(type = "number")) @RequestParam(name = "longitude", defaultValue = "0") Double longitude,
+            @Parameter(description = "Latitude", example = "0", schema = @Schema(type = "number")) @RequestParam(name = "latitude", defaultValue = "0") Double latitude,
+            @Parameter(description = "Rayon en mètre", example = "0", schema = @Schema(type = "number")) @RequestParam(name = "rayon", defaultValue = "0") Double rayon
     ) {
         return "longitude: " + longitude + ", latitude: " + latitude + ", rayon: " + rayon + " m";
     }
-
 }
