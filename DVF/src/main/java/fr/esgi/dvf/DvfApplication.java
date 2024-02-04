efcb49bb6e6692421bc7fed86c5323a17e638161
CHINE Dmitri <dmitri.chine@apave.com>
2024-02-04 16:56:04 +0100

    1. Utilisation Artemis (impl ActiveMQ pour Spring) 2. Utilisation ActiveMQ embarque de jakarta (javax c'est pour spring 2.x.x 3. Ajouté gestionnaire d'erreurs pour JmsContainerFactory 4. PdfGenerationRequest doit etre Serializable pour envoyer vers Jms 5. @EnableJms : onfigurer et de prendre en charge les fonctionnalités JMS dans l'application spring

--- DVF/src/main/java/fr/esgi/dvf/DvfApplication.java
+++ DVF/src/main/java/fr/esgi/dvf/DvfApplication.java
@@ -1,42 +1,75 @@
 package fr.esgi.dvf;
 
+import fr.esgi.dvf.init.DataFonciersDownloadService;
+import fr.esgi.dvf.service.CsvReaderService;
+import jakarta.jms.ConnectionFactory;
 import org.apache.logging.log4j.LogManager;
 import org.apache.logging.log4j.Logger;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.autoconfigure.jms.DefaultJmsListenerContainerFactoryConfigurer;
+import org.springframework.context.annotation.Bean;
+import org.springframework.jms.annotation.EnableJms;
+import org.springframework.jms.config.DefaultJmsListenerContainerFactory;
+import org.springframework.jms.config.JmsListenerContainerFactory;
 import org.springframework.scheduling.annotation.EnableScheduling;
 import org.springframework.scheduling.annotation.Scheduled;
-import fr.esgi.dvf.init.DataFonciersDownloadService;
-import fr.esgi.dvf.service.CsvReaderService;
-import jakarta.annotation.PostConstruct;
+import org.springframework.util.ErrorHandler;
 
 @SpringBootApplication
+@EnableJms
 @EnableScheduling
-public class DvfApplication{
+public class DvfApplication {
 
-    private static final Logger LOGGER = LogManager.getLogger(DvfApplication.class);
+  private static final Logger LOGGER = LogManager.getLogger(DvfApplication.class);
 
-    @Autowired
-    private DataFonciersDownloadService dataFonciersDownloadService;
-    @Autowired
-    private CsvReaderService csvReaderService;
+  @Autowired
+  private DataFonciersDownloadService dataFonciersDownloadService;
+  @Autowired
+  private CsvReaderService csvReaderService;
 
-    public static void main(String[] args) {
-        SpringApplication.run(DvfApplication.class, args);
-    }
+  public static void main(String[] args) {
+    SpringApplication.run(DvfApplication.class, args);
+  }
 
-    @PostConstruct
-    public void init() {
-        dataFonciersDownloadService.downloadAndSaveFile();
+  // @PostConstruct
+  // public void init() {
+  // dataFonciersDownloadService.downloadAndSaveFile();
+  //
+  // dataFonciersDownloadService.unzip();
+  // }
 
-        dataFonciersDownloadService.unzip();
-    }
+  @Scheduled(fixedRate = 300000)
+  public void scheduleSaveToDatabase() {
+    LOGGER.info("Tâche automatique : sauvegarde en base des 100 000 lignes suivantes");
+    csvReaderService.saveToDatabase();
+  }
 
-    @Scheduled(fixedRate = 300000)
-    public void scheduleSaveToDatabase() {
-        LOGGER.info("Tâche automatique : sauvegarde en base des 100 000 lignes suivantes");
-        csvReaderService.saveToDatabase();
-    }
+  @Bean
+  public JmsListenerContainerFactory<?> myFactory(ConnectionFactory connectionFactory,
+                                                  DefaultJmsListenerContainerFactoryConfigurer configurer) {
+
+    DefaultJmsListenerContainerFactory factory = new DefaultJmsListenerContainerFactory();
+
+    ErrorHandler errorHandler = new ErrorHandler() {
+      @Override
+      public void handleError(Throwable t) {
+        LOGGER.atError().log("An error has occurred in the transaction {}",
+                             t.getLocalizedMessage());
+      }
+    };
+
+    // anonymous class
+    factory.setErrorHandler(errorHandler);
+
+    // lambda function
+    // factory.setErrorHandler(t -> LOGGER.atError().log("An error has occurred in the transaction {}",
+    // t.getLocalizedMessage()));
+
+    configurer.configure(factory, connectionFactory);
+
+    return factory;
+  }
 
 }
