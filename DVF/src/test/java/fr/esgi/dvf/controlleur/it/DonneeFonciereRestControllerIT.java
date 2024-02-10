fb9cff9d4aa810e581b65c9dc2783f84f897b367
ChineDmitri <mr.dima11@gmail.com>
2024-02-10 05:48:59 +0100

    Testes unitaire ok

--- DVF/src/test/java/fr/esgi/dvf/controlleur/it/DonneeFonciereRestControllerIT.java
+++ DVF/src/test/java/fr/esgi/dvf/controlleur/it/DonneeFonciereRestControllerIT.java
@@ -1,10 +1,18 @@
 package fr.esgi.dvf.controlleur.it;
 
+import static org.junit.jupiter.api.Assertions.assertTrue;
 import static org.junit.jupiter.api.Assertions.fail;
+import java.nio.file.FileVisitOption;
 import java.nio.file.Files;
 import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.util.concurrent.CompletableFuture;
+import java.util.concurrent.Executors;
+import java.util.concurrent.ScheduledExecutorService;
+import java.util.concurrent.TimeUnit;
 import org.junit.jupiter.api.AfterAll;
 import org.junit.jupiter.api.BeforeAll;
+import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.TestInstance;
 import org.junit.jupiter.api.TestInstance.Lifecycle;
@@ -18,8 +26,6 @@ import org.springframework.test.web.servlet.request.MockMvcRequestBuilders;
 import org.springframework.test.web.servlet.result.MockMvcResultHandlers;
 import org.springframework.test.web.servlet.result.MockMvcResultMatchers;
 
-// @ExtendWith(SpringExtension.class)
-// @WebMvcTest(DonneeFonciereRestControllerIT.class)
 @SpringBootTest
 @AutoConfigureMockMvc
 @IfProfileValue(name = "integration-tests",
@@ -38,28 +44,132 @@ class DonneeFonciereRestControllerIT {
     }
 
     @AfterAll
-    void cleanUp() {
+    void cleanUp() throws Exception {
         this.deleteCsv();
     }
 
 
     @Test
-    void testControllerPDFEndpoint_whenRadius0_shouldNoContent() throws Exception {
+    @DisplayName("Test d'indisponibilité du serveur de fichiers")
+    void testControllerPDFEndpoint_whenFileServerError_thenError5xx() {
         double longitude = 6.019949;
         double latitude = 46.247458;
         double rayon = 1000;
 
-        // Effectuez la requête GET en utilisant les paramètres spécifiés
+        CompletableFuture<Void> clientFuture = CompletableFuture.runAsync(() -> {
+            try {
+                mockMvc.perform(MockMvcRequestBuilders.get("/pdf")
+                                                      .param("longitude", String.valueOf(longitude))
+                                                      .param("latitude", String.valueOf(latitude))
+                                                      .param("rayon", String.valueOf(rayon))
+                                                      .contentType(MediaType.APPLICATION_JSON))
+                       .andDo(MockMvcResultHandlers.print())
+                       .andExpect(MockMvcResultMatchers.status().is5xxServerError());
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+        });
+
+        ScheduledExecutorService executorService = Executors.newSingleThreadScheduledExecutor();
+        CompletableFuture<Void> staticPdfCleanupFuture = CompletableFuture.runAsync(() -> {
+            executorService.scheduleAtFixedRate(() -> cleanUpPdfDirectory(), 0, 1, TimeUnit.MILLISECONDS);
+        });
+
+        CompletableFuture.allOf(clientFuture, staticPdfCleanupFuture)
+                         .thenRun(() -> executorService.shutdownNow())
+                         .join();
+    }
+
+    @Test
+    void testControllerPDFEndpoint_whenRadius0_shouldNoContent() throws Exception {
+        double longitude = 1.0;
+        double latitude = 1.0;
+        double rayon = 0.1;
+
         mockMvc.perform(MockMvcRequestBuilders.get("/pdf")
                                               .param("longitude", String.valueOf(longitude))
                                               .param("latitude", String.valueOf(latitude))
                                               .param("rayon", String.valueOf(rayon))
                                               .contentType(MediaType.APPLICATION_JSON))
                .andDo(MockMvcResultHandlers.print()) // Afficher la sortie pour le débogage
-               .andExpect(MockMvcResultMatchers.status().isOk())
-               .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_PDF));
+               .andExpect(MockMvcResultMatchers.status().isNoContent());
+    }
+
+    @Test
+    void testControllerPDFEndpoint_shouldHaveDifferenceGreaterThan5Seconds() throws Exception {
+        double longitude = 6.019949;
+        double latitude = 46.247458;
+        double rayon = 100000;
+
+        CompletableFuture<Long> client1 = CompletableFuture.supplyAsync(() -> {
+            long startTime = System.nanoTime();
+            try {
+                mockMvc.perform(MockMvcRequestBuilders.get("/pdf")
+                                                      .param("longitude", String.valueOf(longitude))
+                                                      .param("latitude", String.valueOf(latitude))
+                                                      .param("rayon", String.valueOf(rayon))
+                                                      .contentType(MediaType.APPLICATION_JSON))
+                       .andDo(MockMvcResultHandlers.print())
+                       .andExpect(MockMvcResultMatchers.status().isOk())
+                       .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_PDF));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            long endTime = System.nanoTime();
+            return endTime - startTime;
+        });
+
+        CompletableFuture<Long> client2 = CompletableFuture.supplyAsync(() -> {
+            long startTime = System.nanoTime();
+            try {
+                mockMvc.perform(MockMvcRequestBuilders.get("/pdf")
+                                                      .param("longitude", String.valueOf(longitude))
+                                                      .param("latitude", String.valueOf(latitude))
+                                                      .param("rayon", String.valueOf(rayon))
+                                                      .contentType(MediaType.APPLICATION_JSON))
+                       .andDo(MockMvcResultHandlers.print())
+                       .andExpect(MockMvcResultMatchers.status().isOk())
+                       .andExpect(MockMvcResultMatchers.content().contentType(MediaType.APPLICATION_PDF));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            long endTime = System.nanoTime();
+            return endTime - startTime;
+        });
 
 
+        CompletableFuture<Long> combinedFuture = CompletableFuture.allOf(client1, client2)
+                                                                  .thenApplyAsync(ignored -> {
+                                                                      // resultats depuis deux client en asyncronne
+                                                                      Long client1Time = client1.join();
+                                                                      Long client2Time = client2.join();
+
+                                                                      return Math.abs(client1Time - client2Time);
+                                                                  });
+
+        // doit etre different parce que demande de generation pdf est 1 par 1
+        Long difference = combinedFuture.join();
+        System.err.println(difference);
+        assertTrue((double) difference / 1_000_000_000.0 > 5.0);
+    }
+
+    private static void cleanUpPdfDirectory() {
+        try {
+            Path pdfDirectoryPath = Paths.get("static/pdf");
+
+            Files.walk(pdfDirectoryPath, FileVisitOption.FOLLOW_LINKS)
+                 .filter(path -> Files.isRegularFile(path) && path.toString()
+                                                                  .endsWith(".pdf"))
+                 .forEach(file -> {
+                     try {
+                         Files.delete(file);
+                     } catch (Exception e) {
+                         e.printStackTrace();
+                     }
+                 });
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
     }
 
     private void deleteCsv() {
@@ -83,9 +193,9 @@ class DonneeFonciereRestControllerIT {
             }
         }
 
-        // Attendre 30 seconds pour sauvegarder en base des 100 000 lignes
-        try { 
-            Thread.sleep(30000);
+        // Attendre 40 seconds pour sauvegarder en base des 100 000 lignes
+        try {
+            Thread.sleep(40000);
         } catch (InterruptedException e) {
             Thread.currentThread().interrupt();
             return false;
