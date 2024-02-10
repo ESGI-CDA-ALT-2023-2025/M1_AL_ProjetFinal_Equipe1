7873c34a45842cc0c84653621170bed175eee7a8
ChineDmitri <76706642+ChineDmitri@users.noreply.github.com>
2024-02-10 04:07:11 +0100

    Correction faite (#64)

--- dvf-client/src/app/formulaire/formulaire.component.ts
+++ dvf-client/src/app/formulaire/formulaire.component.ts
@@ -1,58 +1,72 @@
 import { Component } from '@angular/core';
-import {MatInputModule} from "@angular/material/input";
-import {FormsModule} from "@angular/forms";
-import {MatButtonModule} from "@angular/material/button";
-import {ApiService} from "../service/api.service";
-import {HttpErrorResponse} from "@angular/common/http";
-import {formatDate} from "@angular/common";
+import { MatInputModule } from '@angular/material/input';
+import { FormsModule } from '@angular/forms';
+import { MatButtonModule } from '@angular/material/button';
+import { ApiService } from '../service/api.service';
+import { HttpErrorResponse } from '@angular/common/http';
+import { formatDate } from '@angular/common';
 
 @Component({
   selector: 'app-formulaire',
   standalone: true,
-  imports: [
-    MatInputModule,
-    FormsModule,
-    MatButtonModule
-  ],
+  imports: [MatInputModule, FormsModule, MatButtonModule],
   templateUrl: './formulaire.component.html',
-  styleUrl: './formulaire.component.scss'
+  styleUrl: './formulaire.component.scss',
 })
 export class FormulaireComponent {
-
   longitude!: number;
   latitude!: number;
   rayon!: number;
 
-  constructor(private _apiService: ApiService) {
-  }
+  messageError?: string;
+
+  constructor(private _apiService: ApiService) {}
 
   onSubmit(): void {
     console.log(
-      "longitude: ", this.longitude,
-      "\nlatitude: ", this.latitude,
-      "\nrayon: ", this.rayon
-    )
+      'longitude: ',
+      this.longitude,
+      '\nlatitude: ',
+      this.latitude,
+      '\nrayon: ',
+      this.rayon
+    );
 
-    this._apiService.getInfo(this.longitude, this.latitude, this.rayon).subscribe({
-      next: (result: Blob): void => {
-        this.afficherPdf(result);
-      },
-      error: (error: HttpErrorResponse): void => {
-        alert(error.message);
-      }
-    });
+    this._apiService
+      .getInfo(this.longitude, this.latitude, this.rayon)
+      .subscribe({
+        next: (result: Blob | null): void => {
+          if (result !== null) {
+            this.afficherPdf(result);
+          } else {
+            this.messageError =
+              'Pas de données pour longitude : ' +
+              this.longitude +
+              ' latitude : ' +
+              this.latitude +
+              ' rayon : ' +
+              this.rayon;
+            console.log('No content received (204)');
+          }
+        },
+        error: (error: HttpErrorResponse): void => {
+          console.log('Erreur : ', error);
+          alert(error.message);
+        },
+      });
   }
 
   afficherPdf(pdfBlobObject: Blob): void {
-
-    let newBlob: Blob = new Blob([pdfBlobObject], { type: "application/pdf" });
+    let newBlob: Blob = new Blob([pdfBlobObject], { type: 'application/pdf' });
 
     const data: string = window.URL.createObjectURL(newBlob);
 
     let link = document.createElement('a');
     link.href = data;
     link.download = this._genererTitreFichier();
-    link.dispatchEvent(new MouseEvent('click', {bubbles: true, cancelable: true, view: window}));
+    link.dispatchEvent(
+      new MouseEvent('click', { bubbles: true, cancelable: true, view: window })
+    );
   }
 
   private _genererTitreFichier(): string {
@@ -66,5 +80,4 @@ export class FormulaireComponent {
 
     return `DonneesFoncieres_${annee}-${mois}-${jour}_${heures}${minutes}`;
   }
-
 }
--- dvf-client/src/app/service/api.service.ts
+++ dvf-client/src/app/service/api.service.ts
@@ -1,6 +1,6 @@
 import { Injectable } from '@angular/core';
 import {HttpClient, HttpResponse} from "@angular/common/http";
-import {Observable} from "rxjs";
+import {Observable, map} from "rxjs";
 
 @Injectable({
   providedIn: 'root'
@@ -11,8 +11,19 @@ export class ApiService {
 
   constructor(private _http: HttpClient) { }
 
-  getInfo(lon: number, lat: number, ray: number): Observable<Blob> {
-    return this._http.get(this._apiUrl + `pdf?longitude=${lon}&latitude=${lat}&rayon=${ray}`, {responseType: 'blob'});
+  getInfo(lon: number, lat: number, ray: number): Observable<Blob | null> {
+    return this._http.get(
+      this._apiUrl + `pdf?longitude=${lon}&latitude=${lat}&rayon=${ray}`,
+      { responseType: 'blob', observe: 'response' } // observe: 'response' pour complet HttpResponse
+    ).pipe(
+      map(response => {
+        if (response.status === 204) {
+          return null; // Si 294, donc null
+        } else {
+          return response.body as Blob;
+        }
+      })
+    );
   }
 
 }
